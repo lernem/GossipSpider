ab573bd63d44f16a5875235317fa8d30ae740998
lernem <lernem@foxmail.com>
2016-05-26 11:16:39 +0800

    beautify test output, tree look read account from config file

--- Test/__init__.py
+++ Test/__init__.py
@@ -8,19 +8,15 @@ reload(sys)
 sys.setdefaultencoding('utf-8')
 
 
-def enum_elem(elem, recur_time=0):
-    recur_time += 1
-    print '--- enter enum_elem ---', recur_time
+def enum_elem(elem, recur_indent=''):
+    recur_indent += '  --  '
     if type(elem) is bs4.element.NavigableString:
-        print '--- out enum_elem ---', recur_time
         return
 
     for child in elem.children:
-        print 222
-        print type(child), child
+        print recur_indent, type(child), child.name, ' ## (', child, ')'
         if type(child) is not bs4.element.NavigableString:
-            enum_elem(child, recur_time)
-    print '--- out enum_elem ---', recur_time
+            enum_elem(child, recur_indent)
 
 
 def generate_gossip(soup):
@@ -29,10 +25,9 @@ def generate_gossip(soup):
         return None
 
     for gossip_data in tmptag:
-        print '================================'
-        print 333
-        enum_elem(gossip_data)
-        return None
+        # print '================================'
+        # enum_elem(gossip_data)
+        print(gossip_data)
 
 if __name__ == '__main__':
     soup = bs4.BeautifulSoup(open('Multi_emoji.html'), 'lxml')
